75095ab8aeb82b907c98a8efcdcf299db9572284
unknown <andrepatrinicola@gmail.com>
2022-05-10 22:12:24 -0300

    Adicionado retorno de lista por json, retorno http004

--- agenda/settings.py
+++ agenda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-^9)_u7&umh6@^15xm6-*b-lxeay-&*mn!9s-8b7sc+iusgv(4q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
